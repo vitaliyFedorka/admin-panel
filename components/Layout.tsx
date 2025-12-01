a397b10dff0a086823a9aa11c81b4e129a51b318
vitaliyfedorka <vitaliy.fedorka@lemberg.co.ua>
2025-12-01 14:17:54 +0100

    Add framer motion and animation to the Sidebar.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -22,7 +22,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
       <Sidebar />
-      <main className="flex-1 p-8 bg-gray-50 min-h-screen">
+      <main className="flex-1 p-8 bg-gray-50 min-h-screen ml-64">
         {children}
       </main>
     </div>
